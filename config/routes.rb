6737b1ab5c73713035621f2e68ea9db43615439d
AlexandreDidion <ale.didion@gmail.com>
2021-03-12 13:10:30 +0100

    Transform Graph model in a service and transform graphcontroller in dijkstrasController + dijkstra service"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get '/direction', to: 'graphs#direction', as: :direction
-  get '/shortest_path', to: 'graphs#shortest_path', as: :shortest_path
+  get '/direction', to: 'dijkstras#direction', as: :direction
+  get '/shortest_path', to: 'dijkstras#shortest_path', as: :shortest_path
 end
