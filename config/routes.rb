017d08d15e6221e95330e0997d908097560326c5
AlexandreDidion <ale.didion@gmail.com>
2021-03-13 17:08:20 +0100

    improves routes and changes controllers/views accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
-  get '/direction', to: 'dijkstras#direction', as: :direction
-  get '/shortest_path', to: 'dijkstras#shortest_path', as: :shortest_path
+  root to: 'countries#index'
+  resources :countries, only: [:index]
+  resources :cities, only: [:index]
+  resource :dijkstra, only: [:show]
 end
