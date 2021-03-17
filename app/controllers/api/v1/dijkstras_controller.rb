e683328c830719b837729d0b5d9864f810ea83fa
AlexandreDidion <ale.didion@gmail.com>
2021-03-17 19:02:08 +0100

    Add validations for dijkstra api"

--- app/controllers/api/v1/dijkstras_controller.rb
+++ app/controllers/api/v1/dijkstras_controller.rb
@@ -1,8 +1,21 @@
 class Api::V1::DijkstrasController < Api::V1::BaseController
+  before_action :set_city, only: [:create]
+
   def create
-    belgium_mapping = DijkstraService.new_graph
-    rough_path, rough_distance = DijkstraService.path_distance(params[:city_start], params[:city_end], belgium_mapping)
-    @path = rough_path.push(params[:city_start]).reverse
-    @distance = rough_distance.fetch(params[:city_end])
+    if @city_start.country == @city_end.country
+      belgium_mapping = DijkstraService.new_graph
+      rough_path, rough_distance = DijkstraService.path_distance(@city_start.name, @city_end.name, belgium_mapping)
+      @path = rough_path.push(@city_start.name).reverse
+      @distance = rough_distance.fetch(@city_end.name)
+    else
+      render json: { error: 'Cities must be from the same country' }
+    end
+  end
+
+  private
+
+  def set_city
+    @city_start = City.find_by(name: params[:city_start])
+    @city_end = City.find_by(name: params[:city_end])
   end
 end
