fe4e433e92e02a6fe37390e767e2ed57d0b265be
AlexandreDidion <ale.didion@gmail.com>
2021-03-19 15:28:41 +0100

    fix basic_auth

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BaseController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-  http_basic_authenticate_with name: ENV['basic_user'], password: ENV['basic_password']
+  http_basic_authenticate_with name: ENV['BASIC_USER'], password: ENV['BASIC_PASSWORD']
 end
