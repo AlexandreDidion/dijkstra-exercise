f93aa83010f0bdca14d3270abc96c26309d270c1
AlexandreDidion <ale.didion@gmail.com>
2021-03-30 14:45:16 +0200

    correct config rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,4 @@
-Rails.application.config.hosts << "*"
+Rails.application.config.hosts << "dijkstralex.herokuapp.com"
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
