8a2f1e09f9ee426d12ff658e79733b0612f902c0
Alexandre Didion <ale.didion@gmail.com>
2021-05-23 22:43:05 +0200

    Add show endpoint for api countries

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -3,7 +3,18 @@ class Api::V1::CountriesController < Api::V1::BaseController
     @countries = Country.all
     options_api = {}
     options_api[:meta] = { total: @countries.count }
-    json_api = CountrySerializer.new(@countries, options_api).serializable_hash.to_json
+    render_json(@countries, options_api)
+  end
+
+  def show 
+    @country = Country.find(params[:id])
+    render_json(@country)
+  end
+
+  private 
+
+  def render_json(countries, options_api={})
+    json_api = CountrySerializer.new(countries, options_api).serializable_hash.to_json
     render json: json_api
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :countries, only: [:index] do
+      resources :countries, only: [:index, :show] do
         resources :cities, only: [:index]
       end
       resources :cities, only: [:index]
