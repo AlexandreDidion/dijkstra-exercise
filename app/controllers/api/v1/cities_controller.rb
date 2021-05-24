ae9450a3a117b00a49efb2c9a06b50ba042c8a31
Alexandre Didion <ale.didion@gmail.com>
2021-05-24 11:09:24 +0200

    add show endpoint to cities

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -3,7 +3,18 @@ class Api::V1::CitiesController < Api::V1::BaseController
     @cities = params[:country_id].nil? ? City.all : City.where("country_id = #{params[:country_id]}")
     options_api = {}
     options_api[:meta] = { total: @cities.count }
-    json_api = CitySerializer.new(@cities, options_api).serializable_hash.to_json
+    render_json(@cities, options_api)
+  end
+
+  def show
+    @city = City.find(params[:id])
+    render_json(@city)
+  end
+
+  private 
+
+  def render_json(cities, options_api = {})
+    json_api = CitySerializer.new(cities, options_api).serializable_hash.to_json
     render json: json_api
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :countries, only: [:index, :show] do
-        resources :cities, only: [:index]
+        resources :cities, only: [:index, :show]
       end
       resources :cities, only: [:index]
       resources :dijkstras, only: [:create]
