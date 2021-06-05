b00dc34b87a38c2adcfa4820b821f2680f4854e1
Alexandre Didion <ale.didion@gmail.com>
2021-06-05 21:15:02 +0200

    add city start and city end id to serializer

--- app/controllers/api/v1/dijkstras_controller.rb
+++ app/controllers/api/v1/dijkstras_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::DijkstrasController < Api::V1::BaseController
   def create
     if @city_start.country == @city_end.country
       @path, @distance = Dijkstras::Create.execute(@city_start, @city_end)
-      json_api = DijkstraSerializer.new(@city_start, { params: { path: @path, distance: @distance }}).serializable_hash.to_json
+      json_api = DijkstraSerializer.new(@city_start, { params: { path: @path, distance: @distance, city_start_id: @city_start.id, city_end_id: @city_end.id }}).serializable_hash.to_json
       render json: json_api
     else
       render json: { error: 'Cities must be from the same country' }
--- app/serializers/dijkstra_serializer.rb
+++ app/serializers/dijkstra_serializer.rb
@@ -10,4 +10,10 @@ class DijkstraSerializer
   attribute :distance_km do |city, params|
     params[:distance]
   end
+  attribute :city_start_id do |city, params|
+    params[:city_start_id]
+  end
+  attribute :city_end_id do |city, params|
+    params[:city_end_id]
+  end 
 end
