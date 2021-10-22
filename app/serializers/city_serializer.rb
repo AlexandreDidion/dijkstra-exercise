d91defbb145c2f9672c898bd1f9453a860a49cb1
AlexandreDidion <ale.didion@gmail.com>
2021-10-22 14:47:44 +0200

    add coordinates to serializer API

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -2,7 +2,7 @@ class CitySerializer
   include JSONAPI::Serializer
   set_type :city
   set_key_transform :dash
-  attribute :name
+  attribute :name, :latitude, :longitude
 
   belongs_to :country, meta: Proc.new { |city_record| { country_name: city_record.country.name } }
   has_many :starting_roads, serializer: RoadSerializer
